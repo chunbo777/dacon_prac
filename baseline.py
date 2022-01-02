4f51ccf67064caff11901401e53f0d8f084a825b
chunbo777 <apppl2cation@naver.com>
2022-01-02 17:28:14 +0900

    m'

--- baseline.py
+++ baseline.py
@@ -9,6 +9,7 @@ import copy
 import gc
 import tqdm
 import glob
+import tweepy
 from konlpy.tag import Mecab
 import torch
 from torch import nn
@@ -71,3 +72,61 @@ train["total"] = train.title + " " + train.region + " " +train.context
 test["total"] = test.title + " " + test.region + " " + test.context
 
 train.head()
+
+encoder_len = 500
+decoder_len = 50
+max_vocab_size = 20000
+batch_size = 32
+num_layers = 6
+d_model = 512
+dff = 2048
+num_heads = 8
+dropout_rate = 0.1
+epochs = 20
+learning_rate = 1e-4
+device = torch.device("cuda:0")
+
+df_train = train.iloc[:-200]
+df_val = train.iloc[-200:]
+
+class Mecab_Tokenizer():
+    def __init__(self, max_length, mode, max_vocab_size=-1):
+        self.text_tokenizer = Mecab()
+        self.mode = mode
+        self.txt2idx = {'pad_':0, 'unk_':1}
+        self.idx2txt = {0:'pad_', 1:'unk_'}
+        self.max_length = max_length
+        self.word_count = {}
+        self.max_vocab_size = max_vocab_size
+        
+        # 띄어쓰기를 찾기 위한 태그 목록
+        self.font_blank_tag = [
+            '', 'EC', 'EC+JKO', 'EF', 'EP+EC', 'EP+EP+EC', 'EP+ETM', 'EP+ETN+JKO', 'ETM', 'ETN', 'ETN+JKO', 'ETN+JX', 'IC', 'JC', 'JKB', 'JKB+JX', 'JKO',
+            'JKQ', 'JKS', 'JX', 'MAG', 'MAG+JX', 'MAG+XSV+EP+EC', 'MAJ','MM', 'MM+EC', 'NNB', 'NNB+JKB', 'NNB+JKO', 'NNB+VCP+EC', 'NNBC', 'NNG', 'NNG+JX+JKO',
+            'NNG+VCP+EC', 'NNP', 'NNP+JX', 'NP', 'NP+JKO', 'NP+JKS', 'NP+JX', 'NP+VCP+EC', 'NR', 'SC', 'SF', 'SL', 'SN', 'SSC', 'SSO', 'SY', 'UNKNOWN',
+            'VA+EC', 'VA+EC+VX+ETM', 'VA+ETM', 'VA+ETN+JKB+JX', 'VCN+EC', 'VCN+ETM', 'VCP', 'VCP+EC', 'VCP+EP+EC', 'VCP+EP+ETM', 'VCP+ETM', 'VCP+ETN',
+            'VV+EC', 'VV+EC+JX', 'VV+EC+VX+EC', 'VV+EC+VX+ETM', 'VV+EP+EC', 'VV+EP+ETM', 'VV+ETM', 'VV+ETN', 'VX+EC', 'VX+EC+VX+EP+EC', 'VX+EP+ETM',
+            'VX+ETM', 'XPN', 'XR', 'XSA+EC', 'XSA+EC+VX+ETM', 'XSA+ETM', 'XSN', 'XSV+EC', 'XSV+EP+EC', 'XSV+ETM', 'XSV+ETN', 'XSV+JKO'
+        ]
+        self.back_blank_tag = [
+            '', 'IC', 'MAG', 'MAG+JX', 'MAG+XSV+EP+EC', 'MAJ', 'MM', 'MM+EC', 'NNB', 'NNB+JKB', 'NNB+VCP', 'NNB+VCP+EC', 'NNB+VCP+EF', 'NNBC', 'NNBC+VCP+EC',
+            'NNG', 'NNG+JC', 'NNG+JX+JKO', 'NNG+VCP', 'NNG+VCP+EC', 'NNG+VCP+ETM', 'NNP', 'NNP+JX', 'NP', 'NP+JKG', 'NP+JKO', 'NP+JKS', 'NP+JX', 'NP+VCP+EC', 'NP+VCP+EF',
+            'NR', 'SC', 'SL', 'SN', 'SSC', 'SSO', 'SY', 'VA', 'VA+EC', 'VA+EC+VX+ETM', 'VA+EF', 'VA+ETM', 'VA+ETN', 'VA+ETN+JKB+JX', 'VCN', 'VCN+EC', 'VCN+EF', 'VCN+ETM',
+            'VCN+ETN', 'VCP', 'VCP+EF', 'VV', 'VV+EC', 'VV+EC+JX', 'VV+EC+VX', 'VV+EC+VX+EC', 'VV+EC+VX+EF', 'VV+EC+VX+EP+EC', 'VV+EC+VX+ETM', 'VV+EF', 'VV+EP', 'VV+EP+EC',
+            'VV+EP+ETM', 'VV+ETM', 'VV+ETN', 'VV+ETN+VCP+EF', 'VX', 'VX+ETM', 'XPN', 'XR', 'XSA+ETN+VCP+EF', 'XSN'
+        ]
+
+        
+    def morpheme(self, sentence_list):
+        new_sentence = []
+        for i, sentence in tqdm(enumerate(sentence_list)):
+            temp = []
+            if self.mode == 'dec':
+                temp.append('sos_')
+            for t in self.text_tokenizer.pos(sentence):
+                temp.append('_'.join(t))
+            if self.mode == 'dec':
+                temp.append('eos_')
+            new_sentence.append(' '.join(temp))
+            
+        return new_sentence
